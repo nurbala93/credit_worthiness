051494eae87d417eacade3f512fa6627298ad867
nurbala93 <43009956+nurbala93@users.noreply.github.com>
2019-05-26 07:41:15 +0600

    Add files via upload

---
+++ AppConfig.java
@@ -0,0 +1,22 @@
+package net.coffeecoding.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("net.coffeecoding")
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+
+}
